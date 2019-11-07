9d885478f6fc2c1a4680fbfcc9fe107c4b3fe32d
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2019-11-07 17:01:10 -0600

    Clases, ejemplo

---
+++ AndroidEmpty/MainActivity.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace AndroidEmpty
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            //Creación de objetos traidos desde la vista
+            Button btn = FindViewById<Button>(Resource.Id.btnConvertir); //Así llamo el objecto al codigo
+            EditText txt = FindViewById<EditText>(Resource.Id.txtTelefono);
+            TextView lbl = FindViewById<TextView>(Resource.Id.lblConvertido);
+            SeekBar SeekBar = FindViewById<SeekBar>(Resource.Id.seekBar1);
+            CalendarView calendario = new CalendarView(this);
+            calendario.DateChange += Calendario_DateChange;
+            //Evento clickdel botton
+            btn.Click += (sender, e) =>
+            {
+                lbl.Text = "Hoola carajitos del señor xd";
+                string translatedText = PhoneTranslator.ToNumber(txt.Text);
+                if (string.IsNullOrWhiteSpace(translatedText))
+                {
+                    lbl.Text = string.Empty;
+                }
+                else
+                {
+                    lbl.Text = translatedText;
+                }
+
+
+                //Android.App.AlertDialog alerta = new Android.App.AlertDialog.Builder(this).Create();
+
+                //alerta.SetTitle("Erros");
+                //alerta.SetMessage("Prueba");
+                //alerta.SetButton("Cancelar",
+                //            (a, b) =>
+                //            {
+
+                //            });
+                //alerta.Show();
+
+            };
+
+        }
+
+        private void Calendario_DateChange(object sender, CalendarView.DateChangeEventArgs e)
+        {
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AndroidEmpty/PhoneTranslator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidEmpty
+{
+    public static class PhoneTranslator
+    {
+        public static string ToNumber(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return "";
+            }
+            else
+            {
+                text = text.ToUpper();
+                var newnumber = new StringBuilder();
+                foreach (var c in text)
+                {
+                    if ("-0123456789".Contains(c))
+                    {
+                        newnumber.Append(c);
+                    }
+                    else
+                    {
+                        var result = TranslateToNumber(c);
+                        if (result!=null)
+                        {
+                            newnumber.Append(result);
+                        }
+                    }
+                }
+                return newnumber.ToString();
+            }
+        }
+
+        private static int? TranslateToNumber(char c)
+        {
+
+            if ("ABC".Contains(c))
+            {
+                return 2;
+            }
+            else if ("DEF".Contains(c))
+            {
+                return 3;
+            }
+            else if ("GHI".Contains(c))
+            {
+                return 4;
+            }
+            else if ("JKL".Contains(c))
+            {
+                return 5;
+            }
+            else if ("MNOÑ".Contains(c))
+            {
+                return 6;
+            }
+            else if ("PQRS".Contains(c))
+            {
+                return 7;
+            }
+            else if ("TUV".Contains(c))
+            {
+                return 8;
+            }
+            else if ("WXYZ".Contains(c))
+            {
+                return 9;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
